0a28dba058b893295af3eca074305f0b8ecb03f4
Ilyoung Jung <ext3fs@hanmail.net>
2021-08-08 02:51:39 +0900

    5

--- summary.c
+++ summary.c
@@ -1,3 +1,9 @@
-initializer list	- p84
-literal pooling		- p130
-raw literal string	- p131
+nested namespace			- p58
+literal(값자체가 이름)			- p59
+fallthrough				- p71
+structured binding			- p81
+initializer list			- p84
+literal pooling				- p130
+raw literal string			- p131
+using namespace std::string_literals	- p136
+string_view				- p141
