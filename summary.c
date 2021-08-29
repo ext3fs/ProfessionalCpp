62f1a6d4c893db98d8a9ae310c5617056c4a8485
Ilyoung Jung <ext3fs@hanmail.net>
2021-08-29 16:26:01 +0900

    8

--- summary.c
+++ summary.c
@@ -1,19 +1,36 @@
-nested namespace			- p58
-literal(값자체가 이름)			- p59
-fallthrough				- p71
-structured binding			- p81
-initializer list			- p84
-literal pooling				- p130
-raw literal string			- p131
-using namespace std::string_literals	- p136
-string_view				- p141
-std::size()				- p281
-decltype()				- p293
-weak_ptr				- p297
-enable_shared_from_this			- p299
-dangling pointer			- p307
-#include "stdafx.h"			- p316
-most vexing parse			- p328
-명시적 디폴트 생성자 			- p330
-명시적 삭제된 생성자			- p331
-
+nested namespace				- p58
+literal(값자체가 이름)				- p59
+fallthrough					- p71
+structured binding				- p81
+initializer list				- p84
+literal pooling					- p130
+raw literal string				- p131
+using namespace std::string_literals		- p136
+string_view					- p141
+std::size()					- p281
+decltype()					- p293
+weak_ptr					- p297
+enable_shared_from_this				- p299
+dangling pointer				- p307
+#include "stdafx.h"				- p316
+most vexing parse				- p328
+explicit default constructor 			- p330
+explicit deleted constructor			- p331
+delegating construtor				- p341
+copy constructor & copy assignment constructor	- p348
+RVO(return value optimization) or copy elision	- p350
+copy and swap					- p366
+rule of five					- p374
+rule of zero					- p378
+mutable						- p382
+const base overloading				- p383
+std::as_const()					- p384
+LTCG(link time code generation)			- p387
+default argument				- p388
+static inline member variable			- p390
+static const member variabel			- p392
+forward declaration				- p394
+explicit					- p402
+epsilon test					- p407
+private implementation idiom, bridge pattern	- p409
+terminate(), set_terminate()			- p653
